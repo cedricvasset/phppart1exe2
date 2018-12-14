3a99db37f551a82d8931ae5ae8da98747ff8a25a
cedricvasset <vasset.cedric@orange.fr>
2018-12-14 15:56:23 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php part1</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <p>
+    <?php
+    $firstname = 'Cédric';
+    $lastname = 'VASSET';
+    $age = 44;
+      echo 'je m\'appelle '. $firstname . ' ' . $lastname . ' ' . $age;
+    ?>
+  </p>
+</body>
+</html>
